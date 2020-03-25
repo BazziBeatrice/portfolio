a2f167c2dc93a110a76e466084bd7268a0bedee9
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-03-25 17:49:37 +0100

    yes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,62 @@
 // JavaScript Document
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 
- $('#fuso').hide();
 
-         $(document).on('mouseenter mouseleave', '#project02', function(){
-    // By giving this function two triggers, the same action is performed for each trigger
-    $('#fuso').toggle();
+
+
+    var yOff = 15;
+    var xOff = -20;
+
+    $(".text-hover-image").hover(function (e) {
+        $("body").append("<h1 id='image-when-hovering-text'><img src='./assets/images/spleen-1.png'/></h1>");
+        $("#image-when-hovering-text")
+            .css("position", "absolute")
+            .css("top", (e.pageY - yOff))
+            .css("left", (e.pageX + xOff))
+            // .fadeIn("fast");
+    }, function () {
+       $("#image-when-hovering-text").remove();
+    });
+
+    $(".text-hover-image").mousemove(function (e) {
+        $("#image-when-hovering-text")
+            .css("top", (e.pageY - yOff))
+            .css("left", (e.pageX + xOff));
+    });
+
 });
 
 
-$(document).ready(function(){
+AOS.init({
+  duration: 1200,
+})
+//
+//  $('#fuso').hide();
+//
+//          $(document).on('mouseenter mouseleave', '#project02', function(){
+//     // By giving this function two triggers, the same action is performed for each trigger
+//     $('#fuso').toggle();
+// });
+
+
 
   //this function makes the category image appear on hover
   // $('.fuso-image').hide();
@@ -41,6 +89,3 @@ $(document).ready(function(){
                 //             $("#makeyourpresence_hover").css("opacity", "0");
                 //             }
                 // );
-
-
-})
