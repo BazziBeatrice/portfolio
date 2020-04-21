fbd2cd8f2a5e79f015d05cd4ee0ac8d42bf85fad
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-04-21 21:57:00 +0200

    ok

--- js/p5/sketch.js
+++ js/p5/sketch.js
@@ -6,9 +6,25 @@ var p2Hover;
 var mouseIsOvering2 = false;
 let img2;
 
+var p3Hover;
+var mouseIsOvering3 = false;
+let img3;
+
+var p4Hover;
+var mouseIsOvering4 = false;
+let img4;
+
+var p5Hover;
+var mouseIsOvering5 = false;
+let img5;
+
 var p6Hover;
 var mouseIsOvering6 = false;
 let img6;
+
+var p7Hover;
+var mouseIsOvering7 = false;
+let img7;
 
 function preload() {}
 
@@ -24,6 +40,17 @@ function setup() {
   img6 = loadImage("https://raw.githubusercontent.com/BazziBeatrice/Portfolio/master/assets/images/contratempo-1.png");
   p6Hover = select("#p06")
 
+  img3 = loadImage("https://i.imgur.com/ze5cSt5.png");
+  p3Hover = select("#p03")
+
+  img4 = loadImage("https://raw.githubusercontent.com/BazziBeatrice/Portfolio/master/assets/images/contratempo-1.png");
+  p4Hover = select("#p04")
+
+  img5 = loadImage("https://i.imgur.com/ze5cSt5.png");
+  p5Hover = select("#p05")
+
+  img7 = loadImage("https://raw.githubusercontent.com/BazziBeatrice/Portfolio/master/assets/images/contratempo-1.png");
+  p7Hover = select("#p07")
 
 }
 
@@ -33,9 +60,19 @@ function draw() {
     mostraImmagine();
   } else if (mouseIsOvering2 == true) {
       mostraImmagine2();
-    }else if (mouseIsOvering6 == true) {
+    }
+    else if (mouseIsOvering3 == true) {
+        mostraImmagine3();
+      }  else if (mouseIsOvering4 == true) {
+            mostraImmagine4();
+          }else if (mouseIsOvering5 == true) {
+                mostraImmagine5();
+              }
+      else if (mouseIsOvering6 == true) {
         mostraImmagine6();
-      }
+      }  else if (mouseIsOvering7 == true) {
+          mostraImmagine7();
+        }
     else {
     clear();
   }
@@ -48,8 +85,20 @@ function mouseStatus(status) {
 function mouseStatus2(status) {
   mouseIsOvering2 = status;
 }
+function mouseStatus3(status) {
+  mouseIsOvering3 = status;
+}
+function mouseStatus4(status) {
+  mouseIsOvering4 = status;
+}
+function mouseStatus5(status) {
+  mouseIsOvering5 = status;
+}
 function mouseStatus6(status) {
   mouseIsOvering6 = status;
+}
+function mouseStatus7(status) {
+  mouseIsOvering7 = status;
 }
 
 function mostraImmagine() {
@@ -60,6 +109,18 @@ function mostraImmagine2() {
   // image(img, mouseX - (img.width / 2),(img.height / 2));
   image(img2, mouseX, mouseY + 20, (img.width / 2), (img.height / 2))
 }
+function mostraImmagine3() {
+  image(img3, mouseX, mouseY + 20, (img.width / 2), (img.height / 2))
+}
+function mostraImmagine4() {
+  image(img4, mouseX, mouseY + 20, (img.width / 2), (img.height / 2))
+}
+function mostraImmagine5() {
+  image(img5, mouseX, mouseY + 20, (img.width / 2), (img.height / 2))
+}
 function mostraImmagine6() {
   image(img6, mouseX, mouseY + 20, (img.width / 2), (img.height / 2))
+}
+function mostraImmagine7() {
+  image(img7, mouseX, mouseY + 20, (img.width / 2), (img.height / 2))
 }
