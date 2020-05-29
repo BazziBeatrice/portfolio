f2485fda0c155788f7307568955891a627ba9238
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-05-29 17:35:40 +0200

    ok

--- js/main-phone.js
+++ js/main-phone.js
@@ -1,50 +1,133 @@
 // JavaScript Document
 
-if($(window).width() < 768){
-
-$("#image-phone-02").hide();$("#image-phone-03").hide();$("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();$("#image-phone-07").hide();
-
-window.addEventListener('scroll', function() {
-
-       $("#image-phone-01").show();$("#image-phone-02").hide();$("#image-phone-03").hide();$("#image-phone-04").hide();
-       $("#image-phone-05").hide();$("#image-phone-06").hide();$("#image-phone-07").hide();
-
-  if (document.body.scrollTop > 330 || document.documentElement.scrollTop > 330) {
-    $("#image-phone-01").hide();$("#image-phone-02").show();$("#image-phone-03").hide();
-    $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();$("#image-phone-07").hide();
-     }
-      if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-        $("#image-phone-01").hide();$("#image-phone-02").hide(); $("#image-phone-03").show();
-        $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();
-        $("#image-phone-07").hide();
-        }
-        if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
-          $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
-          $("#image-phone-04").show();$("#image-phone-05").hide();$("#image-phone-06").hide();
-          $("#image-phone-07").hide();
-          }
-          if (document.body.scrollTop > 1620 || document.documentElement.scrollTop > 1620) {
-                  $("#image-phone-01").hide();$("#image-phone-02").hide();  $("#image-phone-03").hide();
-                  $("#image-phone-04").hide();$("#image-phone-05").show();$("#image-phone-06").hide();
-                  $("#image-phone-07").hide();
-             }
-              if (document.body.scrollTop > 1900 || document.documentElement.scrollTop > 1900) {
-                $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
-                $("#image-phone-04").hide();  $("#image-phone-05").hide();  $("#image-phone-06").show();
-                $("#image-phone-07").hide();
-                }
-                if (document.body.scrollTop > 2400 || document.documentElement.scrollTop > 2400) {
-                       $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
-                       $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();
-                     $("#image-phone-07").show();
-                  }
-                  if (document.body.scrollTop > 2900 || document.documentElement.scrollTop > 2900) {
-                    $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
-                    $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();
-                    $("#image-phone-07").hide();
-                    }
-});
-}
+if ($(window).width() < 768) {
+  $("#image-phone-01").hide();
+  $("#image-phone-02").hide();
+  $("#image-phone-03").hide();
+  $("#image-phone-04").hide();
+  $("#image-phone-05").hide();
+  $("#image-phone-06").hide();
+  $("#image-phone-07").hide();
+  $(window).scroll(function() {
+    $("#image-phone-01").show();
+    $("#image-phone-02").hide();
+    $("#image-phone-03").hide();
+    $("#image-phone-04").hide();
+    $("#image-phone-05").hide();
+    $("#image-phone-06").hide();
+    $("#image-phone-07").hide();
+    if ($(this).scrollTop() > 330) {
+      $('#image-phone-02').show();
+      $("#image-phone-01").hide();
+      $("#image-phone-03").hide();
+      $("#image-phone-04").hide();
+      $("#image-phone-05").hide();
+      $("#image-phone-06").hide();
+      $("#image-phone-07").hide();
+    }
+    if ($(this).scrollTop() > 700) {
+      $("#image-phone-01").hide();
+      $("#image-phone-02").hide();
+      $("#image-phone-03").show();
+      $("#image-phone-04").hide();
+      $("#image-phone-05").hide();
+      $("#image-phone-06").hide();
+      $("#image-phone-07").hide();
+    }
+    if ($(this).scrollTop() > 1200) {
+      $("#image-phone-01").hide();
+      $("#image-phone-02").hide();
+      $("#image-phone-03").hide();
+      $("#image-phone-04").show();
+      $("#image-phone-05").hide();
+      $("#image-phone-06").hide();
+      $("#image-phone-07").hide();
+    }
+    if ($(this).scrollTop() > 1620) {
+      $("#image-phone-01").hide();
+      $("#image-phone-02").hide();
+      $("#image-phone-03").hide();
+      $("#image-phone-04").hide();
+      $("#image-phone-05").show();
+      $("#image-phone-06").hide();
+      $("#image-phone-07").hide();
+    }
+    if ($(this).scrollTop() > 1900) {
+      $("#image-phone-01").hide();
+      $("#image-phone-02").hide();
+      $("#image-phone-03").hide();
+      $("#image-phone-04").hide();
+      $("#image-phone-05").hide();
+      $("#image-phone-06").show();
+      $("#image-phone-07").hide();
+    }
+    if ($(this).scrollTop() > 2400) {
+      $("#image-phone-01").hide();
+      $("#image-phone-02").hide();
+      $("#image-phone-03").hide();
+      $("#image-phone-04").hide();
+      $("#image-phone-05").hide();
+      $("#image-phone-06").hide();
+      $("#image-phone-07").show();
+    }
+    if ($(this).scrollTop() > 2900) {
+      $("#image-phone-01").hide();
+      $("#image-phone-02").hide();
+      $("#image-phone-03").hide();
+      $("#image-phone-04").hide();
+      $("#image-phone-05").hide();
+      $("#image-phone-06").hide();
+      $("#image-phone-07").hide();
+    }
+  });
+    }
+//
+//
+// if($(window).width() < 768){
+//
+// $("#image-phone-02").hide();$("#image-phone-03").hide();$("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();$("#image-phone-07").hide();
+//
+// window.addEventListener('scroll', function() {
+//
+//        $("#image-phone-01").show();$("#image-phone-02").hide();$("#image-phone-03").hide();$("#image-phone-04").hide();
+//        $("#image-phone-05").hide();$("#image-phone-06").hide();$("#image-phone-07").hide();
+//
+//   if (document.body.scrollTop > 330 || document.documentElement.scrollTop > 330) {
+//     $("#image-phone-01").hide();$("#image-phone-02").show();$("#image-phone-03").hide();
+//     $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();$("#image-phone-07").hide();
+//      }
+//       if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+//         $("#image-phone-01").hide();$("#image-phone-02").hide(); $("#image-phone-03").show();
+//         $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();
+//         $("#image-phone-07").hide();
+//         }
+//         if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+//           $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
+//           $("#image-phone-04").show();$("#image-phone-05").hide();$("#image-phone-06").hide();
+//           $("#image-phone-07").hide();
+//           }
+//           if (document.body.scrollTop > 1620 || document.documentElement.scrollTop > 1620) {
+//                   $("#image-phone-01").hide();$("#image-phone-02").hide();  $("#image-phone-03").hide();
+//                   $("#image-phone-04").hide();$("#image-phone-05").show();$("#image-phone-06").hide();
+//                   $("#image-phone-07").hide();
+//              }
+//               if (document.body.scrollTop > 1900 || document.documentElement.scrollTop > 1900) {
+//                 $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
+//                 $("#image-phone-04").hide();  $("#image-phone-05").hide();  $("#image-phone-06").show();
+//                 $("#image-phone-07").hide();
+//                 }
+//                 if (document.body.scrollTop > 2400 || document.documentElement.scrollTop > 2400) {
+//                        $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
+//                        $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();
+//                      $("#image-phone-07").show();
+//                   }
+//                   if (document.body.scrollTop > 2900 || document.documentElement.scrollTop > 2900) {
+//                     $("#image-phone-01").hide();$("#image-phone-02").hide();$("#image-phone-03").hide();
+//                     $("#image-phone-04").hide();$("#image-phone-05").hide();$("#image-phone-06").hide();
+//                     $("#image-phone-07").hide();
+//                     }
+// });
+// }
 
 
 //LOADER
