b18a3b16e5fe50081e7a85942c4fb8f940926aa8
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2020-06-01 12:58:00 +0200

    ok

--- js/main-phone.js
+++ js/main-phone.js
@@ -81,18 +81,3 @@ if ($(window).width() < 768) {
     }
   });
     }
-
-
-//LOADER
-// var loader = document.getElementById("loader");
-// window.setTimeout(function(){loader.style.height="500px";loader.style.width="500px";loader.style.visibility ="hidden";}, 2000);
-
-/* Open */
-// function openNav() {
-//   document.getElementById("myNav").style.display = "block";
-// }
-
-/* Close */
-// function closeNav() {
-//   document.getElementById("myNav").style.display = "none";
-// }
